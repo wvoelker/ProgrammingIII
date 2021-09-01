cdc7fbb11f7d38a563a750a6468e7079b84b94f9
wvoelker <wvoelker@uncc.edu>
2021-09-01 13:19:03 -0400

    assignment

---
+++ Assignment1.java
@@ -0,0 +1,12 @@
+//Random Sequence Generator
+//WILLIAM VOELKER
+package git_programmingIII;
+
+public class Assignment1 
+{
+	public static int x = 0;
+	public static void main(String[] args)
+    {
+		System.out.println(x);
+	}
+}
