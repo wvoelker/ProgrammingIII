9afa935e409e8792d15f0fabc01e1faebc9098bf
wvoelker <wvoelker@uncc.edu>
2021-09-01 16:28:13 -0400

    Assignment 1 upload: random 3 mer generator

--- Assignment1.java
+++ Assignment1.java
@@ -35,19 +35,20 @@ public class Assignment1
 	                        }
 			}
 		if(sequence.equals("AAA")){AAAcount++;};
-		System.out.println(counter1);
+		//System.out.println(counter1);
 		System.out.println(sequence);
 		}
 //QUESTION 2: PRINT THE COUNT OF AAA
 //WE WOULD EXPECT THIS COUNT TO EQUAL TO .25*.25*.25*1000 = 15.625
-		System.out.println(AAAcount);
+		System.out.println("AAA COUNT IS: ");
+                System.out.println(AAAcount);
 //QUESTION 3: MODIFIED FREQUENCY TEST
 //p(A) = .12
 //p(C) = .38
 //p(G) = .39
 //p(T) = .11
-                int AAAcount = 0;
-                Random random = new Random();
+                AAAcount = 0;
+                random = new Random();
                 for(int counter1=0;counter1<1000;counter1++)
                 {
                         String sequence = "";
@@ -55,27 +56,30 @@ public class Assignment1
                         for(int counter=0;counter<3;counter++)
                         {
                                 int randomizer = random.nextInt(100);
-                                if(randomizer >= 0 and randomizer <= 11)
+                                if(randomizer >= 0 && randomizer <= 11)
                                 {
                                         sequence = sequence + "A";
                                 }
-                                if(randomizer >= 12 and randomizer <= 49)
+                                if(randomizer >= 12 && randomizer <= 49)
                                 {
                                         sequence = sequence + "T";
                                 }
-                                if(randomizer >= 50 and randomizer <= )
+                                if(randomizer >= 50 && randomizer <= 88)
                                 {
                                         sequence = sequence + "G";
                                 }
-                                if(randomizer == 3)
+                                if(randomizer >= 89 && randomizer <= 99)
                                 {
                                         sequence = sequence + "C";
                                 }
                         }
                 if(sequence.equals("AAA")){AAAcount++;};
-                System.out.println(counter1);
+                //System.out.println(counter1);
                 System.out.println(sequence);
                 }
+//THE MODIFIED EXPECTED AAA COUNT WOULD BE .12*.12*.12*1000 = 1.728
+                System.out.println("REVISED AAA COUNT IS: ");
+                System.out.println(AAAcount);
 
 	}
 }
