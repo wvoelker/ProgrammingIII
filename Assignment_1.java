204761042eb8eb6c2bb223cab8fff35235365899
wvoelker <wvoelker@uncc.edu>
2021-09-01 12:35:39 -0400

    added assignment

---
+++ Assignment_1.java
@@ -0,0 +1,11 @@
+//HELLO WORLD PROGRAM
+//WILLIAM VOELKER
+package git_programmingIII;
+
+public class HelloWorld 
+{
+	public static void main(String[] args)
+        {
+		System.out.println("Hello, World!");
+	}
+}
