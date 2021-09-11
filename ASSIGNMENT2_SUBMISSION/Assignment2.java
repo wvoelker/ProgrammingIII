7e9b2dd9a98fbadb79abe9999334dcdb339d8054
wvoelker <wvoelker@uncc.edu>
2021-09-11 17:40:27 -0400

    modified assignment 2

--- ASSIGNMENT2_SUBMISSION/Assignment2.java
+++ ASSIGNMENT2_SUBMISSION/Assignment2.java
@@ -4,16 +4,35 @@ import java.util.Random;
 import java.util.Calendar;
 import java.util.Date;
 import java.time.ZonedDateTime;
+import java.util.Scanner;
 
 public class Assignment2
 {
-        public static void lister(String[] listee)
+        public static boolean start()
 	{
-		for (int x = 0; x < 20; x++)
+		while(true)
 		{
-			System.out.println(listee[x]);
+		System.out.println("AMINO ACID QUIZ READY, TYPE S TO START");
+		Scanner start_input = new Scanner(System.in);
+		String check = start_input.next();
+		if(check.equals("S") || check.equals("s"))
+			{
+				return true;
+			}
 		}
-
+	}
+	
+	public static void quiz()
+	{
+		long startTime = System.currentTimeMillis();
+		long time = 0;
+		short num_correct = 0;
+		while((time-startTime) < 30000)
+			{
+				time = System.currentTimeMillis();
+				System.out.println((time-startTime)/1000);
+			}
+		System.out.println("TIME'S UP");
 	}
 	public static void main(String[] args)
         {
@@ -32,7 +51,10 @@ public class Assignment2
 		"serine","threonine","tryptophan",
 		"tyrosine", "valine"};
 
-		lister(SHORT_NAMES);
+		if (start()==true)
+			{
+				quiz();
+			}
 
 	}
 }
