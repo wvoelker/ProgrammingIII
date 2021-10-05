19bec32567e569963eb68615b787d304ad134b59
wvoelker <wvoelker@uncc.edu>
2021-10-05 19:23:36 -0400

    finished assignment 4

--- Assignment4_submission/FastaSequence.java
+++ Assignment4_submission/FastaSequence.java
@@ -19,14 +19,14 @@ public class FastaSequence
 			BufferedReader reader = new BufferedReader(new FileReader(new File(filepath)));
 			String line = new String();
 			int increment = 0;
-			List<FastaSequence> final_list = new LinkedList<>();
+			List<FastaSequence> final_list = new ArrayList<>();
 			List<String> sequence_list = new LinkedList<>();
 			String temp_header = new String();
 			while(line != null)
 				{
 					line = reader.readLine();
-					
-					if(Character.valueOf(line.charAt(0)).equals('>'))
+					System.out.println(line);
+					if(line==null||Character.valueOf(line.charAt(0)).equals('>'))
 						{
 							if(increment != 0)
 								{
@@ -46,6 +46,7 @@ public class FastaSequence
 					increment++;
 				}
 			reader.close();
+			
 			return final_list;
 
 		}
@@ -66,24 +67,60 @@ public class FastaSequence
 			char basepair = 'x';
 			float GC_counter = 0;
 			for(int x=0; x<GC_list.length; x++)
-				basepair = this.sequence.charAt(x);
-				if(Character.valueOf(basepair).equals('G')||Character.valueOf(basepair).equals('C')||Character.valueOf(basepair).equals('g')||Character.valueOf(basepair).equals('c'))
-					{
-						GC_counter++;
-					}
+				{
+					basepair = this.sequence.charAt(x);
+					if(Character.valueOf(basepair).equals('G')||Character.valueOf(basepair).equals('C')||Character.valueOf(basepair).equals('g')||Character.valueOf(basepair).equals('c'))
+						{
+							GC_counter++;
+						}
+				}
 			float GC_ratio = GC_counter/(Integer.valueOf(sequence.length()).floatValue());
 			return GC_ratio;
+		}
+	public int getlettercount(char letter)
+		{
+                        char basepair = 'x';
+                        int counter = 0;
+                        for(int x=0; x<this.sequence.length(); x++)
+				{
+                               		basepair = this.sequence.charAt(x);
+                                	if(Character.valueOf(basepair).equals(letter)||Character.valueOf(basepair).equals(Character.toLowerCase(letter)))
+                                                {	
+							counter++;
+						}
+                                }
+			return counter;
 		}
 	
 	public static void writeTableSummary( List<FastaSequence> list, File outputFile) throws Exception
 		{
+			BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+			String headers = "sequenceID	numA	numC	numG	numT	sequence\n";
+			writer.write(headers);
+			String seqid = new String();
+			String letA = new String();
+			String letT = new String();
+			String letC = new String();
+			String letG = new String();
+			String seq = new String();
+			for(FastaSequence x : list)
+			{
+				seqid = x.getHeader();
+				letA = String.valueOf(x.getlettercount('A'));
+				letT = String.valueOf(x.getlettercount('T'));
+				letG = String.valueOf(x.getlettercount('G'));
+				letC = String.valueOf(x.getlettercount('C'));
+				seq = x.sequence;
+				writer.append(seqid + '\t' + letA + '\t' + letT + '\t' + letG + '\t' + letC + '\t' + seq + '\n');
+			}
+			writer.close();
 		}
 
 	public static void main(String[] args) throws Exception
 		{
 		     List<FastaSequence> fastaList = 
 			FastaSequence.readFastaFile(
-				"c:\\pointsToSome\\FastaFile.txt");
+				"./fasta_example.txt");
 
 		     for( FastaSequence fs : fastaList)
 		     {
@@ -92,7 +129,7 @@ public class FastaSequence
 		         System.out.println(fs.getGCRatio());
 		      }
 
-		     File myFile = new File("c:\\yourFilePathHere\\out.txt");
+		     File myFile = new File("./fasta_output.txt");
 
 		     writeTableSummary( fastaList,  myFile);
 	
